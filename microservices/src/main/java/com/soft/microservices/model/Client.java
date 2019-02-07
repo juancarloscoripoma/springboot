44d378ce9088781ab407fc048c946ea0c031869e
jcarlos <computer_programme@hotmail.com>
2019-02-07 17:37:06 -0400

    changed to spring boot 1.9.1

--- microservices/src/main/java/com/soft/microservices/model/Client.java
+++ microservices/src/main/java/com/soft/microservices/model/Client.java
@@ -1,7 +1,19 @@
 package com.soft.microservices.model;
 
+/**
+ * Created by jcarlos on 07/02/2019.
+ */
+import jdk.nashorn.internal.objects.annotations.Getter;
+
 import javax.persistence.*;
 import java.io.Serializable;
+
+/*
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(exclude = {"economicActivity","billingMode","invoices"})
+*/
 
 @Entity
 @Table(name = "clients")
