f761209465a6b025d366cf450d0dead1b9c669b9
jcarlos <computer_programme@hotmail.com>
2019-02-18 19:04:29 -0400

    added stereotype

--- demoMysql/src/main/java/com/soft/demoMysql/repository/BookRepository.java
+++ demoMysql/src/main/java/com/soft/demoMysql/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.soft.demoMysql.repository;
 
 import com.soft.demoMysql.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
 }
